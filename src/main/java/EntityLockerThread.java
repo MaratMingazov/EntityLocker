e066098ce132e617d899744e5fed6f606e46509b
MaratMingazov <maratmingazovr@gmail.com>
2021-04-05 12:40:46 +0300

    Implement EntityLockerThread

---
+++ src/main/java/EntityLockerThread.java
@@ -0,0 +1,82 @@
+import candyjar.util.concurrent.locks.EntityLocker;
+
+public class EntityLockerThread<T> extends Thread {
+
+    private EntityLocker<T> locker;
+    private T key;
+    private final int timeout = 5000;
+
+    public EntityLockerThread(final EntityLocker<T> entityLocker, final T entityKey) {
+        locker = entityLocker;
+        key = entityKey;
+    }
+
+    public final void run() {
+        executeLock();
+        executeTryLock();
+        executeTryLock(3000);
+        executeGlobalLock();
+    }
+
+    public final void executeLock() {
+        try {
+            locker.lock(key);
+            System.err.println(Thread.currentThread().getName() + " started protected code on entity " + key);
+            Thread.sleep(timeout);
+            // execute protected code here
+        } catch (InterruptedException e) {
+            currentThread().interrupt();
+        } finally {
+            System.err.println(Thread.currentThread().getName() + " finished protected code on entity " + key);
+            locker.unlock(key);
+        }
+    }
+
+    public final void executeTryLock() {
+        try {
+            if (locker.tryLock(key)) {
+                System.err.println(Thread.currentThread().getName() + " started protected code on entity " + key);
+                Thread.sleep(timeout);
+                // execute protected code here
+            } else {
+                System.err.println(Thread.currentThread().getName() + " refused protected code on entity " + key);
+            }
+        } catch (InterruptedException e) {
+            currentThread().interrupt();
+        } finally {
+            System.err.println(Thread.currentThread().getName() + " finished protected code on entity " + key);
+            locker.unlock(key);
+        }
+    }
+
+    public final void executeTryLock(final long timeout) {
+        try {
+            if (locker.tryLock(key, timeout)) {
+                System.err.println(Thread.currentThread().getName() + " started protected code on entity " + key);
+                Thread.sleep(this.timeout);
+                // execute protected code here
+            } else {
+                System.err.println(Thread.currentThread().getName() + " refused protected code on entity " + key);
+            }
+        } catch (InterruptedException e) {
+            currentThread().interrupt();
+        } finally {
+            System.err.println(Thread.currentThread().getName() + " finished protected code on entity " + key);
+            locker.unlock(key);
+        }
+    }
+
+    public final void executeGlobalLock() {
+        try {
+            locker.globalLock();
+            System.err.println(Thread.currentThread().getName() + " started global protected code");
+            Thread.sleep(timeout);
+            // execute protected code here
+        } catch (InterruptedException e) {
+            currentThread().interrupt();
+        } finally {
+            System.err.println(Thread.currentThread().getName() + " finished global protected code");
+            locker.globalUnlock();
+        }
+    }
+}
